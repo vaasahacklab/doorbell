f8a923aa1a1b8e63a3cf3fc0f56e9a19528513da
Sami Olmari <sami@olmari.fi>
2020-07-06 02:55:45 +0300

    Complete rewamp, use python 3.
    Signed-off-by: Sami Olmari <sami@olmari.fi>

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# doorbell
-Doorbell script to play sound
+# doorbell #
+Doorbell script to play sound when Hacklab "doorbell" is triggered
+
+## Installation
+
+System prerequirents:
+libasound2-dev
+python3-dev
+
+For Debian-based systems run: `< requirements.apt xargs sudo apt install -y`
+
+Setup venv:
+`python3 -m venv venv`
+
+Activate venv:
+`source ./venv/bin/activate`
+
+Install python modules
+`pip install -r requirements.txt`
+
+## Config
+
+Config.json.example provides example config, copy it to config.json and modify for your enviroment.
+
+Configuration parameters and their meaning:
+
+`"host": "localhost"`, hostname or IP to listen on
+`"port": 8088`, port to listen on
+`"dooropen": null`, sound filename that plays when door is open
+`"doorbell": "2.wav"`, sound filename that plays when doorbell is ringed
+
+Either sound file setting can be set to `null` but not both together.
--- doorbell.py
+++ doorbell.py
@@ -1,28 +1,80 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-from subprocess import call
-import socket
-#create an INET, STREAMing socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, \
-                                socket.IPPROTO_TCP)
-#bind the socket to a public host,
-# and a well-known port
-serversocket.bind(("musicbox.lan", 8080))
-#become a server socket
-serversocket.listen(1)
+import logging, logging.config
+logging.config.fileConfig("logging.ini")
 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+import sys
+import json
+import simpleaudio
 
-while 1:
-    #accept connections from outside
-    (clientsocket, address) = serversocket.accept()
-    #now do something with the clientsocket
-        # Clearly state that connection will be closed after this response,
-    # and specify length of response body
-    clientsocket.send("HTTP/1.1 200 OK\n"
-         +"Content-Type: text/html\n"
-         +"Content-Length: 27\n"
-         +"\n" # Important!
-         +"<html><body></body></html>\n");
-    clientsocket.close()
+__all__ = ["Doorbell"]
 
-    call(["aplay","/root/doorbell.wav"])
+log = logging.getLogger("Doorbell")
+
+# Load config from file
+log.debug("Loading config file")
+try:
+    with open(os.path.join(sys.path[0], "config.json"), "r") as f:
+        config = json.load(f)
+    f.close()
+except ValueError as e:
+    log.critical("config.json is malformed, got error:\n\t" + str(e))
+    f.close()
+except Exception as e:
+    log.critical("Failed loading config file, got error:\n\t" + str(e))
+    quit()
+
+class Doorbell(BaseHTTPRequestHandler):
+    def do_PUT(self):
+        try: 
+            content_len = int(self.headers.get('Content-Length'))
+            data = json.loads(self.rfile.read(content_len))
+            if data['dooropened']:
+                log.info("Got request for successful door open")
+                self.send_response(204)
+                self.end_headers()
+                self.playAudio(config['dooropen'])
+            elif data['dooropened'] is False:
+                log.info("Got request for doorbell")
+                self.send_response(204)
+                self.end_headers()
+                self.playAudio(config['doorbell'])
+        except Exception as e:
+            log.error("Got invalid request, propably incorrect parameter in request, needs JSON \"dooropened\" true/false")
+            self.send_response(400)
+            self.end_headers()
+
+    def playAudio(self, file):
+        if file is None:
+            log.info("Audio disabled for request, skipping play")
+        if file is not None:
+            log.info("Playing audio for request")
+            wave_obj = simpleaudio.WaveObject.from_wave_file(file)
+            play_obj = wave_obj.play()
+            play_obj.wait_done()
+
+if __name__ == '__main__':
+    if config['dooropen'] is None and config['doorbell'] is None:
+        log.critical("Both dooropen and doorbell setting is disabled, quitting.")
+        quit()
+
+    if not config['dooropen'] is None:
+        if not os.path.exists(os.path.join(config['dooropen'])):
+            log.critical("Door open enabled and sound file for door open doesn't exists")
+            quit()
+    if not config['doorbell'] is None:
+        if not os.path.exists(os.path.join(config['doorbell'])):
+            log.critical("Doorbell sound enabled and sound file for doorbell doesn't exists")
+            quit()
+
+    httpd = HTTPServer((config['host'], config['port']), Doorbell)
+    log.info("Starting")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    log.info("Stopped")
